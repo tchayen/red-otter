70e763e442b0fd3185ad6e76995eb68671720c02
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2024-02-02 23:00:34 +0200

    fix: load color attachment in render pass instead of clearing
    This is a breaking change but required to make it possible to use red-otter as an UI overlay in WebGPU apps. This probaly requires updating docs and guides as well.

--- src/renderer/WebGPURenderer.ts
+++ src/renderer/WebGPURenderer.ts
@@ -589,11 +589,7 @@ export class WebGPURenderer implements Renderer {
     const renderPass = commandEncoder.beginRenderPass({
       colorAttachments: [
         {
-          // TODO release: this should be changed to load and clearing should be done outside.
-          // Otherwise UI will steal the whole screen.
-          clearValue: { a: 1, b: 0, g: 0, r: 0 },
-          loadOp: "clear",
-          // loadOp: "load",
+          loadOp: "load",
           resolveTarget: this.context
             .getCurrentTexture()
             .createView({ label: "antialiased resolve target" }),
